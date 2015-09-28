1d70aafc5d0fc6b78d5a76650d97bbe47a6e2cc8
ponugubati <pramadasu@gmail.com>
2015-09-28 08:26:29 +0530

    Landmarklist

---
+++ BeeCarService/Models/Landmark.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeeCarService.Models
+{
+    public class Landmark
+    {
+        public int ID { get; set; }
+        public string LandmarkLocation { get; set; }
+    }
+}
